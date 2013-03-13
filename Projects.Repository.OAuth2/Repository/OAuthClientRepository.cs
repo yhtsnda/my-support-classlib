f73878de992c47f6ae7a8c0be3f7f8056ab6132d
iamgoingin123@gmail.com <iamgoingin123@gmail.com@95cf7758-7330-533a-9251-d6995be482a1>
2013-03-13 04:47:27 +0000

    dsdd

---
+++ Projects.Repository.OAuth2/Repository/OAuthClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using Projects.Framework;
+using Projects.Framework.OAuth2;
+
+namespace Nd.Repository.OAuth2
+{
+    public class OAuthClientRepository : AbstractShardRepository<OAuthClient>, IOAuthClientRepository
+    {
+
+    }
+}
