9217f26714573769c8230b92bf61de973b97366a
iamgoingin123@gmail.com <iamgoingin123@gmail.com@95cf7758-7330-533a-9251-d6995be482a1>
2013-03-13 04:47:27 +0000

    dsdd

---
+++ trunk/Projects.Repository.OAuth2/Mapping/AccessTokenMapping.cs
@@ -0,0 +1,35 @@
+﻿using FluentNHibernate.Mapping;
+using Projects.Framework;
+using Projects.Framework.OAuth2;
+
+namespace Nd.Repository.OAuth2
+{
+    public class AccessTokenMapping : ClassMap<AccessToken>
+    {
+        public AccessTokenMapping()
+        {
+            Cache.NonStrictReadWrite();
+            Table("oauth2_accesstoken");
+
+            Id(o => o.Id);
+            Map(o => o.UserId);
+            Map(o => o.ClientId);
+            Map(o => o.Token);
+            Map(o => o.Expire);
+            Map(o => o.IssueTime);
+        }
+    }
+
+    public class AccessTokenDefine : ClassDefine<AccessToken>
+    {
+        public AccessTokenDefine()
+        {
+            Id(o => o.Id);
+            Map(o => o.ClientId);
+            Map(o => o.UserId);
+            Map(o => o.Token);
+            Map(o => o.IssueTime);
+            Map(o => o.Expire);
+        }
+    }
+}
---
+++ trunk/Projects.Repository.OAuth2/Mapping/OAuthClientMapping.cs
@@ -0,0 +1,35 @@
+﻿using FluentNHibernate.Mapping;
+using Projects.Framework;
+using Projects.Framework.OAuth2;
+
+namespace Nd.Repository.OAuth2
+{
+    public class OAuthClientMapping : ClassMap<OAuthClient>
+    {
+        public OAuthClientMapping()
+        {
+            Cache.NonStrictReadWrite();
+            Table("oauth2_client");
+
+            Id(o => o.Id);
+            Map(o => o.Name);
+            Map(o => o.Key);
+            Map(o => o.Secret);
+            Map(o => o.Description);
+
+        }
+    }
+
+    public class OAuthClientDefine : ClassDefine<OAuthClient>
+    {
+        public OAuthClientDefine()
+        {
+            Id(o => o.Id);
+            Map(o => o.Name);
+            Map(o => o.Description);
+            Map(o => o.Key);
+            Map(o => o.Secret);
+
+        }
+    }
+}
