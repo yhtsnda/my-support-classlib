bb9828f1f604c190727778ac82996c2890b450ec
iamgoingin123@gmail.com <iamgoingin123@gmail.com@95cf7758-7330-533a-9251-d6995be482a1>
2013-07-10 11:26:55 +0000

    saved

---
+++ Projects.Access/MongoAccess/MongoServerDetector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Projects.Framework.Utils;
+
+namespace Projects.Framework.MongoAccess
+{
+    public class MongoServerDetector : AbstractServerDetector
+    {
+        protected override void OnDetect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
