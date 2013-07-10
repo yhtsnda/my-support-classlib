0e8686504bb4473d7ab29821f102ae10f3558fbf
iamgoingin123@gmail.com <iamgoingin123@gmail.com@95cf7758-7330-533a-9251-d6995be482a1>
2013-07-10 11:27:11 +0000

    saved

---
+++ trunk/Projects.Framework/Utils/AbstractServerDetector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projects.Framework.Utils
+{
+    public abstract class AbstractServerDetector
+    {
+        protected static DateTime ServerInitTime;
+
+        static AbstractServerDetector()
+        {
+            ServerInitTime = DateTime.Now;
+        }
+
+        public virtual int DueSecond { get { return 200; } }
+
+        protected abstract void OnDetect();
+
+        public void Detect()
+        {
+            if (DateTime.Now.AddSeconds(-DueSecond) > ServerInitTime)
+            {
+                OnDetect();
+            }
+        }
+    }
+}
