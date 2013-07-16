586a41688bea8aeb0d16ee49cbf33860d597eb22
iamgoingin123@gmail.com <iamgoingin123@gmail.com@95cf7758-7330-533a-9251-d6995be482a1>
2013-07-16 10:29:46 +0000

    saved

---
+++ Projects.UserCenter/Services/ClientAppService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Projects.Framework;
+
+namespace Projects.UserCenter
+{
+    /// <summary>
+    /// 客户端服务
+    /// </summary>
+    public class ClientAppService
+    {
+        private IClientAppRepository clientRepository;
+
+        public ClientAppService(IClientAppRepository clientRepository)
+        {
+            this.clientRepository = clientRepository;
+        }
+
+        /// <summary>
+        /// 创建接入应用信息
+        /// </summary>
+        /// <param name="client">客户端信息</param>
+        public ClientApp CreateClientApp(ClientApp client)
+        {
+            clientRepository.Create(client);
+            return client;
+        }
+
+        /// <summary>
+        /// 修改接入应用的信息
+        /// </summary>
+        /// <param name="client"></param>
+        public void ModifyClientAppInfo(ClientApp client)
+        {
+        }
+
+        /// <summary>
+        /// 将制定的应用无效化
+        /// </summary>
+        /// <param name="clientId">应用ID</param>
+        public void DisabledClientApp(int clientId)
+        {
+        }
+
+        /// <summary>
+        /// 将指定的应用有效
+        /// </summary>
+        /// <param name="clientId">应用ID</param>
+        public void EnabledClientApp(int clientId)
+        {
+
+        }
+
+        /// <summary>
+        /// 将指定的应用设置为限制
+        /// </summary>
+        /// <param name="clientId">应用ID</param>
+        public void LimitedClientApp(int clientId)
+        {
+
+        }
+
+        /// <summary>
+        /// 刷新应用的应用秘钥
+        /// </summary>
+        /// <param name="clientId">应用ID</param>
+        /// <returns>新的应用秘钥</returns>
+        public string RefreshClientAppSecrect(int clientId)
+        {
+        }
+
+        public ClientApp GetClientApp(int clientId)
+        {
+
+        }
+
+        public ClientApp GetClientAppByName(string appName)
+        {
+
+        }
+
+        /// <summary>
+        /// 获取接入的所有应用
+        /// </summary>
+        /// <param name="onlyEnabled">只获取启用应用</param>
+        /// <returns>应用列表</returns>
+        public IList<ClientApp> GetClientAppList(bool onlyEnabled = true)
+        {
+            var spec = clientRepository.CreateSpecification();
+            if (onlyEnabled)
+                return clientRepository.FindAll(spec.Where(o => o.Status == ClientAppStatus.Enabled));
+            return clientRepository.FindAll(spec);
+        }
+    }
+}
--- Projects.UserCenter/Services/UserService.cs
+++ Projects.UserCenter/Services/UserService.cs
@@ -26,8 +26,8 @@ namespace Projects.UserCenter
         /// <summary>
         /// 根据ID获取用户信息
         /// </summary>
-        /// <param name="userId"></param>
-        /// <returns></returns>
+        /// <param name="userId">用户ID</param>
+        /// <returns>用户信息</returns>
         public User GetUser(int userId)
         {
             return userRepository.Get(userId);
@@ -36,8 +36,8 @@ namespace Projects.UserCenter
         /// <summary>
         /// 根据用户名获取用户信息
         /// </summary>
-        /// <param name="userName"></param>
-        /// <returns></returns>
+        /// <param name="userName">用户名</param>
+        /// <returns>用户信息</returns>
         public User GetUserByName(string userName)
         {
             var spec = userRepository.CreateSpecification().Where(o => o.UserName == userName);
@@ -47,8 +47,8 @@ namespace Projects.UserCenter
         /// <summary>
         /// 根据用户ID获取用户列表
         /// </summary>
-        /// <param name="ids"></param>
-        /// <returns></returns>
+        /// <param name="ids">用户ID列表</param>
+        /// <returns>用户信息列表</returns>
         public IList<User> GetUserList(IEnumerable<int> ids)
         {
             return userRepository.GetList(ids);
@@ -57,8 +57,8 @@ namespace Projects.UserCenter
         /// <summary>
         /// 更加用户名获取用户列表
         /// </summary>
-        /// <param name="names"></param>
-        /// <returns></returns>
+        /// <param name="names">用户名列表</param>
+        /// <returns>用户信息列表</returns>
         public IList<User> GetUserListByName(IEnumerable<string> names)
         {
             var spec = userRepository.CreateSpecification().Where(o => names.Contains(o.UserName));
@@ -171,23 +171,63 @@ namespace Projects.UserCenter
         #endregion
 
         #region 用户信息修改
+        /// <summary>
+        /// 用户密码修改
+        /// </summary>
+        /// <param name="userId">用户ID</param>
+        /// <param name="oldPwd">原始密码</param>
+        /// <param name="newPwd">新密码</param>
+        /// <returns>操作结果</returns>
         public ResultWrapper<ModifyResultCode> ModifyPassword(int userId, string oldPwd, string newPwd)
         {
             var user = userRepository.Get(userId);
             if (user.Status == UserStatus.Disabled)
                 return CreateModifyResult(ModifyResultCode.UserHasDisabled, "该用户已经被禁用,不能修改密码");
 
-            throw new NotImplementedException();
+            if(user.Password.Equals(oldPwd, StringComparison.InvariantCultureIgnoreCase))
+                return CreateModifyResult(ModifyResultCode.PasswordNoMatch, "旧密码错误,无法修改");
+            //这样认为没有加密
+            if (newPwd.Length < 30)
+                newPwd = UserCenterUtility.EncryptPassword(newPwd);
+            user.Password = newPwd;
+            userRepository.Update(user);
+
+            return CreateModifyResult(ModifyResultCode.Success, "用户密码修改成");
         }
 
-        public ResultWrapper<ModifyResultCode> ModifyUser()
+        /// <summary>
+        /// 修改用户信息
+        /// </summary>
+        /// <param name="user">用户对象</param>
+        public void ModifyUser(User user)
         {
-            throw new NotImplementedException();
+            Arguments.NotNull(user, "user");
+
+            var dbUser = userRepository.Get(user.UserId);
+            if (!String.Equals(dbUser.Password, user.Password, StringComparison.InvariantCultureIgnoreCase))
+                throw new ArgumentException("ModifyUser方法不能用于修改用户密码,请使用ModifyPassword方法进行修改");
+            userRepository.Update(user);
         }
 
-        public ResultWrapper<ModifyResultCode> DeleteUser()
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="userId">用户ID</param>
+        public void DeleteUser(int userId)
         {
-            throw new NotImplementedException();
+            var user = userRepository.Get(userId);
+            if (user != null)
+                DeleteUser(user);
+        }
+
+        /// <summary>
+        /// 删除用户信息
+        /// </summary>
+        /// <param name="user">用户对象</param>
+        public void DeleteUser(User user)
+        {
+            Arguments.NotNull(user, "user");
+            userRepository.Delete(user);
         }
         #endregion
 
