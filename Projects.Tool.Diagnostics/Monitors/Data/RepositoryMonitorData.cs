1237d4d257248bd11d9f415c335951a2e8340e5b
iamgoingin123@gmail.com <iamgoingin123@gmail.com@95cf7758-7330-533a-9251-d6995be482a1>
2013-07-30 06:36:58 +0000

    将 Framework中的Monitor移动到Diagnostics

---
+++ Projects.Tool.Diagnostics/Monitors/Data/RepositoryMonitorData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projects.Tool.Diagnostics
+{
+    public class RepositoryMonitorData
+    {
+        public string Name { get; set; }
+
+        public int Count;
+
+        public int CacheHits;
+
+        public int Spans;
+    }
+
+}
