5ceec0a65383c389551183484144f917af86c3c7
iamgoingin123@gmail.com <iamgoingin123@gmail.com@95cf7758-7330-533a-9251-d6995be482a1>
2013-07-31 13:22:22 +0000

    saved

---
+++ trunk/Nova.Config/AbstractConfigLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nova.Config
+{
+    public abstract class AbstractConfigLoader : IConfigLoader
+    {
+
+        public TConfig LoadSingle<TConfig>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, TConfig> LoadMutilPair<TConfig>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TConfig> LoadMutilList<TConfig>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/Nova.Config/IConfigLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nova.Config
+{
+    internal interface IConfigLoader
+    {
+        TConfig LoadSingle<TConfig>(string key);
+        IDictionary<string, TConfig> LoadMutilPair<TConfig>();
+        IList<TConfig> LoadMutilList<TConfig>();
+    }
+}
