39c95745a90c65fc16eef435fd05f945f5cfd20b
iamgoingin123@gmail.com <iamgoingin123@gmail.com@95cf7758-7330-533a-9251-d6995be482a1>
2013-08-02 03:57:46 +0000

    saved

---
+++ trunk/Nova.Route/SinglePartitionFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nova.Route
+{
+    public class SinglePartitionFunction<TKey>: AbstractPartitionFunction
+    {
+        private Func<TKey, int> partitionFun;
+
+        public SinglePartitionFunction(Func<TKey, int> partitionFun, 
+            int[] partitionCount, int[] partitionLength)
+        {
+            if (partitionFun == null)
+                throw new ArgumentNullException("partitionFun");
+
+            this.partitionFun = partitionFun;
+        }
+
+        protected override int Partition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
